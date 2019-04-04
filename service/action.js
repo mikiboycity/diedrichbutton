3626cedca4d0aae4b6440aae2b79a7db5c410fdf
Andrey G <mr.andrey.g.f@gmail.com>
2019-04-04 23:01:59 +0300

    #3 [put test handler for action trigger]

--- service/action.js
+++ service/action.js
@@ -1,5 +1,48 @@
 'use strict';
 
+/**
+ * Action handler to be triggered when the end user will emit an event from his IoT enterprise button
+ *
+ * This will create a Salesforce case for the owner of button
+ */
+const SES = require('aws-sdk/clients/ses');
+const ses = new SES({
+  region: 'us-west-2',
+  apiVersion: '2010-12-01'
+});
+
 module.exports.handler = async event => {
   console.log('event', JSON.stringify(event));
+
+  var email = event.placementInfo.attributes.email;
+  var dsn = event.placementInfo.attributes.dsn;
+
+  var params = {
+    Destination: {
+      ToAddresses: [email]
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: `You have clicked a button: ${dsn}`
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Button event'
+      }
+    },
+    // Source: 'donotreply@diedrichroasters.com'
+    Source: 'mr.andrey.g.f@gmail.com'
+  };
+
+  try {
+    await ses.sendEmail(params).promise();
+  } catch (error) {
+    console.error(JSON.stringify(error));
+    throw error;
+  }
+
+  console.log('Successfully, processed the event');
 };
